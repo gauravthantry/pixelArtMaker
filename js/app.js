c8fc30f9b7862826058089d73cc03b0f112b22d7
GauravThantry <gaurav.thantry@gmail.com>
2018-03-17 18:17:11 +0530

    Fixed a bug. Closed canvas tag in css

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-$("#grid-input").on("click",function(){
-   var rows = $("#row").val();
-   var cols = $("#col").val();
-   for(var i=1;i<=rows;i++){
-       for(var j=1;j<=cols;j++){
-           $(".drawing-area").append('<canvas id="myPixel"></canvas>');
-       }
-   }
+$(document).ready(function(){
+   $("#grid-input").click(function(){
+	   $(".drawing-area").empty();
+	   
+      var rows = $("#row").val();
+      var cols = $("#col").val();
+	  if(rows>0 && cols > 0){
+		  
+      for(var i=1;i<=rows;i++){
+          for(var j=1;j<=cols;j++){
+              $(".drawing-area").append('<canvas id="myPixel" width="20px" height="20px" style="border: 1px solid #000;"></canvas>');
+          }
+   	   $(".drawing-area").append('<br/>');
+      }
+	  }
+	  else{
+		  alert("You haven't provided the grid size!");
+	  }
+   });
 });
