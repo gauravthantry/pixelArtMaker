e5e1e5101fb725e281811b3c724738d53b1ea650
GauravThantry <gaurav.thantry@gmail.com>
2018-03-18 17:00:59 +0530

    Added script to fill in the colours in the grid

--- js/app.js
+++ js/app.js
@@ -5,10 +5,9 @@ $(document).ready(function(){
       var rows = $("#row").val();
       var cols = $("#col").val();
 	  if(rows>0 && cols > 0){
-		  
       for(var i=1;i<=rows;i++){
           for(var j=1;j<=cols;j++){
-              $(".drawing-area").append('<canvas id="myPixel" width="20px" height="20px" style="border: 1px solid #000;"></canvas>');
+              $(".drawing-area").append('<td id="myPixel" width="20px" height="20px" style="border: 1px solid #000;"></td>');
           }
    	   $(".drawing-area").append('<br/>');
       }
@@ -17,4 +16,29 @@ $(document).ready(function(){
 		  alert("You haven't provided the grid size!");
 	  }
    });
+   var color=  $("input[name='color']:checked").val();
+   console.log(color);
+ $("td").click(function(){
+ var color=  $("input[name='color']:checked").val();
+ if(color==='blue')
+ {
+	 if($(this).hasClass('colorFill-Blue'))
+		 $(this).removeClass('colorFill-Blue');
+	  else
+		  $(this).addClass('colorFill-Blue');
+ }
+ else if(color==='Ggreen')
+ {
+	 if($(this).hasClass('colorFill-Green'))
+		 $(this).removeClass('colorFill-Green');
+	 else
+		 $(this).addClass('colorFill-Green');
+ }
+ else{
+	 if($(this).hasClass('colorFill-Yellow'))
+		 $(this).removeClass('colorFill-Yellow');
+	 else
+		 $(this).addClass('colorFill-Yellow');
+ }
+ });
 });
